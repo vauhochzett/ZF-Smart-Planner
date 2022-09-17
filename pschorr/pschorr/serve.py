d006e037fe57b4620596b0d72f4b3f54b4ffdede
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2022-09-17 14:04:06 +0200

    Adds basic bottle serve functionality + static files

--- pschorr/pschorr/serve.py
+++ pschorr/pschorr/serve.py
@@ -1,11 +1,28 @@
 """Main entrypoint to start the server."""
-from bottle import route, run, template
+from pathlib import Path
+from typing import Optional
+
+from bottle import route, run, static_file, template
+
+# Ensure that static files are accessible
+static_dir: Optional[Path] = None
+for static_path in (search_dirs := ["./static", "../static", "./pschorr/static"]):
+    if (the_dir := Path(static_path)).exists():
+        static_dir = the_dir
+if static_dir is None:
+    raise IOError(f"Could not find static dir. Searched directories: {search_dirs}")
 
 
 @route("/")
 def index():
-    """Returns the homepage."""
+    """Return the homepage."""
     return template("<b>Hello, {{ name }}</b>!", name="world")
+
+
+@route("/static/<ext>/<filename>")
+def serve_static(ext, filename):
+    """Return arbitrary static files."""
+    return static_file(filename, root=f"./static/{ext}/")
 
 
 run(host="localhost", port=8975)
